6f75471385c136d56c2b0e6c471bfaa005e3d97e
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-01 11:50:29 -0800

    Putting config.vm.provision into VagrantAdditionalConfig.rb doesn't work

--- VagrantAdditionalConfig.rb
+++ VagrantAdditionalConfig.rb
@@ -12,18 +12,4 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   if Vagrant.has_plugin?("vagrant-omnibus")
     config.omnibus.cache_packages = true
   end
-
-  config.vm.provision "shell", inline: <<-SHELL
-	sudo sh -c 'echo network.host: 0.0.0.0 >>/etc/elasticsearch/elasticsearch.yml'
-    sudo service elasticsearch restart
-
-    sudo apt-get install --yes kibana
-    sudo sed -i.bak "s/^#server.host: \"localhost\"/server.host: \"0.0.0.0\"/" /etc/kibana/kibana.yml
-	sudo sed -i.bak "s/^#elasticsearch.username: .*/elasticsearch.username: \"elastic\"/" /etc/kibana/kibana.yml
-    sudo sed -i.bak "s/^#elasticsearch.password: .*/elasticsearch.password: \"changeme\"/" /etc/kibana/kibana.yml
-    sudo service kibana restart
-
-    sudo service elasticsearch restart
-    sudo service kibana restart
-  SHELL
 end
